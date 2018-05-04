e2ff446d2c65138c38db6724b7aae0a44432944c
Marzio Mastrorilli <marzio-97@hotmail.com>
2018-05-04 12:18:36 +0200

    MODELS IMPLEMENTATI

---
+++ AssociazioneSportiva/AssociazioneSportiva/Models/Categoria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssociazioneSportiva.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("* Descrizione categoria: ")]
+        public string Descrizione { get; set; }
+
+        [Required]
+        [DisplayName("* Età minima: ")]
+        public int EtàMin { get; set; }
+
+        [Required]
+        [DisplayName("* Età massima: ")]
+        public int EtàMax { get; set; }
+    }
+}
---
+++ AssociazioneSportiva/AssociazioneSportiva/Models/Tessera.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssociazioneSportiva.Models
+{
+    public class Tessera
+    {
+        [Required]
+        [DisplayName("* Numero della tessera: ")]
+        public int NumeroTessera { get; set; }
+
+        [Range(typeof(DateTime), "1/1/1800", "1/1/2018")]
+        [DisplayName("* Data di rilascio: ")]
+        public DateTime DataRilascio { get; set; }
+
+        [Required]
+        [DisplayName("* Categoria di appartenenza: ")]
+        public Categoria Categoria { get; set; }
+
+        [Required]
+        [DisplayName("* Persona: ")]
+        public Persona Persona { get; set; }
+    }
+}
